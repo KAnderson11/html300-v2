77199b3c6619d85b1d9317dbe49d93d6e660c423
KAnderson <kylaa2@uw.edu>
2024-02-17 10:51:01 -0800

    created html paragraph element with class of output. created javascript to handle the input. created javascript to handle the output. displayed the answer on the webpage after the machine completed the calculations

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -31,3 +31,29 @@ document. addEventListener("DOMContentLoaded", function(){
   })
 
 })
+
+// Code that requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+
+// Find the gum form, and handle its submit event
+  const gumForm = document.querySelector('#gum-pack')
+
+  gumForm.addEventListener('submit', function(event){
+  // Prevent the form from trying to submit to a server
+     event.preventDefault()	
+    console.log("Submitted!" )
+  
+  //Handle input. The website user will input information about how many packs of gum they want to purchase.
+  const userInput = document.querySelector('#gum-pack .input').value
+  const userNumber = parseInt(userInput)
+    console.log(userNumber)
+  
+  
+  //Handle output
+  const output = document.querySelector('#gum-pack .output')
+  
+  //Output to the webpage. The machine will calculate the total amount owed by multiplying the input information of the number of gum packs by the 3 dollar per pack price. Then the machine will display the total amount owed.
+  output.innerHTML = `${userNumber * 3} Dollars US is the total amount owed`
+  })
+  
+  })
