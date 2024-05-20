7fa332cc0b8044cfd13a606e8e7abc04721b00c5
KAnderson <kylaa2@uw.edu>
2024-05-20 07:47:15 -0700

    deleted the alert-box. deleted the v-show. deleted the button information. Created new button information with an onclick for toggleBorder. created information in my return for showBorder. created information in my methods so I have a Boolean and toggleBorder will either show the border or not show the border depending on if the button has been clicked.created class information for imageBorder to showBorder inside of my img tag within my template info. In the style tags I created a border for my img item and then created styling for items that have been assigned a class of .imageBorder thus I should have a red border appear around the image when the button has been clicked.

--- lesson07/assignment/vue-hwk/vue-project/src/components/ImgComp.vue
+++ lesson07/assignment/vue-hwk/vue-project/src/components/ImgComp.vue
@@ -6,19 +6,22 @@ default {
  name:'ImgComp',
  props:{
 // I had the wrong syntax here with the [] instead of {}.  I also validated the types as String
-   imagepath:
-String,
-   alttext:
-String
+   imagepath: String, alttext: String
  },
  data()
  {
  return{
-   ok:true
+   ok:true,
+
+   showBorder:false
  }
 },
 methods:{
    //can put toggle here
+
+   toggleBorder(){
+    this.showBorder=!this.showBorder
+   }
 }
 }
 
@@ -31,35 +34,33 @@ methods:{
     <div>
       <div class="alert-box"v-show="ok">
     <strong>Click the toggle button </strong>
-	
-  <button @click="ok = !ok">toggle</button>
+    <button @click="toggleBorder()">toggle</button>
+    <h1>Hello! Some text here</h1>
 
-	<h1 v-show="ok">Hello!</h1>
-	
 
-    <br/>
-   
   
  <div>
    <img
  :src="imagepath"
  :alt="alttext"
- :title="alttext">
+ :title="alttext"
+ :class="{ imageBorder:showBorder}"
+ />
    <caption>This is an image</caption>
  </div>
  </div>
 </div>
 </template>
 
-<style scoped>
+<style>
+img{
+border:2px solid transparent;
 
-
-
-.alert-box {
-  color: #666;
-  border: 1px solid black;
-  border-radius: 4px;
-  padding: 20px;
-  background-color: #f8f8f8;
+/* Prevents jump when the order is added */
 }
+
+.imageBorder{
+border:2px solid red;
+}
+
 </style>
