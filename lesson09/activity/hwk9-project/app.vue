60329b26ad24ec46d6ca7d30d3ea21c3cdc36da1
KAnderson <kylaa2@uw.edu>
2024-05-27 21:09:50 -0700

    Installed the Nuxt 3 software. In my VSCode text editor I made changes to my work inside of my hwk9-project folder. In VSCode text editor created pages directory. Inside of the pages directory I have my .vue files. Also in VSCode text editor I created a components folder and I created an assets folder

---
+++ lesson09/activity/hwk9-project/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home text here</NuxtLink>
+      <NuxtLink to="/weather">Weather text here</NuxtLink>
+      <NuxtLink to="/contact">Contact text here</NuxtLink>
+    </nav>
+   <NuxtPage/>
+  </div>
+</template>
---
+++ lesson09/activity/hwk9-project/pages/weather.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+    <h1>Weather Page</h1>
+</div>
+
+</template>
