ebc8b2ca1f6b749d42ef7e7ad2b228fd2562b921
KAnderson <kylaa2@uw.edu>
2024-05-29 19:32:15 -0700

    Created an images folder inside my assets folder. Places several images inside the images folder. Put the image src information into my app.vue and into my birders.vue, also I decided to change my nav bar theme inside of my app.vue so the nav bar with be the dark theme.

--- lesson09/activity/hwk9-project/app.vue
+++ lesson09/activity/hwk9-project/app.vue
@@ -1,6 +1,9 @@
 <template>
+  
+ <img src="~/assets/images/small-logo.png" alt="mint green background with black text that says Tahoma Bird Alliance.org and a illustrated Towhee bird image of a bird with black back feathers and white as well as orange chest feathers" />
+
   <div>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- lesson09/activity/hwk9-project/pages/birders.vue
+++ lesson09/activity/hwk9-project/pages/birders.vue
@@ -1,4 +1,10 @@
 <template>
+<img src="~/assets/images/tall-bird.png" alt="text here"/>
+
+<img src="~/assets/images/wetland-center.png" alt="text here"/>
+
+<img src="~/assets/images/heron.png" alt="text here"/>
+  
 <div>
     <h1>Birders Page</h1>
 </div>
